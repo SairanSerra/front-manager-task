11fbecacd762b0abc22da212d2dddaf1063109e3
Sairan Serra <sairan.serra@gmail.com>
2023-10-15 23:02:02 -0300

    fix - next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  swcMinify: true,
+  output: 'standalone',
+  reactStrictMode: false,
+  antd: {
+    import: true,
+  },
+  webpack: (config, options) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
+}
 
 module.exports = nextConfig
